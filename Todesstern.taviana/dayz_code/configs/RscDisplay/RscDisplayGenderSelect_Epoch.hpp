365338e41faa5d61104575bf36aafcb31dcef8a2
MrFranky <SuperGrobie88@web.de>
2017-11-02 00:03:16 +0100

    Korrektur

--- Todesstern.taviana/dayz_code/configs/RscDisplay/RscDisplayGenderSelect_Epoch.hpp
+++ Todesstern.taviana/dayz_code/configs/RscDisplay/RscDisplayGenderSelect_Epoch.hpp
@@ -9,7 +9,7 @@
 **                                ╚═╝    ╚═════╝ ╚═════╝ ╚══════╝╚══════╝╚══════╝   ╚═╝   ╚══════╝╚═╝  ╚═╝╚═╝  ╚═══╝                                **
 **                                                                                      powered by Chaotentrupp.net                                 **
 **      Script:      RscDisplayGenderSelect_epoch.hpp                                                                                               **
-**      Credits to:  Dayz Dev Team                                                                                                                  **
+**      Credits to:  Dayz Epoch Team                                                                                                                  **
 **      Source:      SQF/dayz_code/Configs/RscDisplay/RscDisplayGenderSelect.hpp                                                                    **
 **                                                                                                                                                  **
 ******************************************************************************************************************************************************
